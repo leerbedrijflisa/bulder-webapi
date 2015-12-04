de0c2b8e8b140945f737ecdb39e2d1b311180483
Mike Faro <ivyk96@live.com>
2015-12-04 09:28:53 +0100

    Added validation for the required fields.

--- Lisa.Bulder.WebApi/TransferObjects/PostedChannel.cs
+++ Lisa.Bulder.WebApi/TransferObjects/PostedChannel.cs
@@ -1,9 +1,14 @@
-﻿namespace Lisa.Bulder.WebApi
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lisa.Bulder.WebApi
 {
     public class PostedChannel
     {
+        [Required]
         public string Administrators { get; set; }
+        [Required]
         public string Authors { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
--- Lisa.Bulder.WebApi/TransferObjects/PostedMessage.cs
+++ Lisa.Bulder.WebApi/TransferObjects/PostedMessage.cs
@@ -1,9 +1,13 @@
-﻿namespace Lisa.Bulder.WebApi
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lisa.Bulder.WebApi
 {
     // This is the message that clients send to the Web API.
     public class PostedMessage
     {
+        [Required]
         public string Text { get; set; }
+        [Required]
         public string Author { get; set; }
     }
 }
