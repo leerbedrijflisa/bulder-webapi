37ccf21bd15937f7b94009ceed5d198595a7ac8a
Mike Faro <ivyk96@live.com>
2015-12-16 10:28:18 +0100

    Removed unnecessary code.

--- Lisa.Bulder.WebApi/Data/ChannelData.cs
+++ Lisa.Bulder.WebApi/Data/ChannelData.cs
@@ -8,17 +8,9 @@ namespace Lisa.Bulder.WebApi
     {
         public async Task<object> FetchChannel(string channel, string id)
         {
-            //var operation = TableOperation.Retrieve<ChannelEntity>(id, null);
-            //var result = await _channels.ExecuteAsync(operation);
-            //return Mapper.ToChannel(result.Result);
-
-            var operation2 = TableOperation.Retrieve<MessageEntity>(channel, id);
-            var result2 = await _messages.ExecuteAsync(operation2);
-            return Mapper.ToMessage(result2.Result);
-
-            //var query = new TableQuery<ChannelEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, channel.ToLower()));
-            //var segment = await _channels.ExecuteQuerySegmentedAsync(query, null);
-            //return Mapper.ToChannel(segment.Results);
+            var operation = TableOperation.Retrieve<MessageEntity>(channel, id);
+            var result = await _messages.ExecuteAsync(operation);
+            return Mapper.ToMessage(result.Result);
         }
 
         public async Task<IEnumerable<object>> FetchChannels()
